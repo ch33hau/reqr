8436ce77f091c515a6519c1378dd39af8630176e
Chee Hau Lim <ch33hau@gmail.com>
2022-11-01 10:02:54 +0100

    try actix

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,16 @@
-use std::error::Error;
 use clap::Parser;
-
-type MyResult<T> = Result<T, Box<dyn Error>>;
+use std::io::Result;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
     /// Target URL
     #[arg(short, long)]
-    url: String,
+    pub url: String,
 }
 
-pub fn run() -> MyResult<()> {
+pub fn run() -> Result<Args> {
     let args = Args::parse();
     println!("Target URL: {}", args.url);
-    Ok(())
+    Ok(args)
 }
